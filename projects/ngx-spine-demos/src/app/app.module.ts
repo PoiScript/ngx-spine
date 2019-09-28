69f1bdbcd24852b83a5fa547e10598e2f72cb3ad
PoiScript <poiscript@gmail.com>
2019-09-28 15:29:15 +0800

    feat: update ngx-spine-demoes

--- projects/ngx-spine-demos/src/app/app.module.ts
+++ projects/ngx-spine-demos/src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatSelectModule } from "@angular/material/select";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSliderModule } from "@angular/material/slider";
+
+import { NgxSpineModule } from "ngx-spine";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatSliderModule,
+    NgxSpineModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
